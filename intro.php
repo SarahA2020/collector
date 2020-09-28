f879dbaa2dd0fd5c107404766cc7e15c01b8ceeb
Sarah <sairs_77@hotmail.com>
2020-09-28 16:14:36 +0100

    indented html as per Marcus's review

--- intro.php
+++ intro.php
@@ -1,21 +1,23 @@
 !DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>AFL Player Cards Collection</title>
-    <!--<link href="normalise.css" type="text/css" rel="stylesheet">
-    <link href="style.css" type="text/css" rel="stylesheet"> -->
-    <meta name=’viewport” content=”width=device-width, initial-scale=0.1”>
-</head>
-<body>
-<nav>
-    <a href="#">Home</a>
-    <a href="#">Collection</a>
-</nav>
+    <head>
+        <meta charset="utf-8">
+        <title>AFL Player Cards Collection</title>
+        <!--<link href="normalise.css" type="text/css" rel="stylesheet">
+        <link href="style.css" type="text/css" rel="stylesheet"> -->
+        <meta name=’viewport” content=”width=device-width, initial-scale=0.1”>
+    </head>
+    <body>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Collection</a>
+        </nav>
 
-<section class="intro">
-    <h1>AFL Player Cards Collection</h1>
+        <section class="intro">
+            <h1>AFL Player Cards Collection</h1>
 
-    <p>Welcome to my awesome collection of Australian Rules Football player cards. Click on 'Collection' to browse through. Each card displays the photo and stats of the some of the best players in the league.</p>
-</section>
+            <p>Welcome to my awesome collection of Australian Rules Football player cards. Click on 'Collection' to browse through. Each card displays the photo and stats of the some of the best players in the league.</p>
+     </section>
+    </body>
+</html>body
 <?php
