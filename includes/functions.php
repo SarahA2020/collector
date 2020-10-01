79ec3d2ac7d46a354d55591a4cf41b9202adb7de
Sarah <sairs_77@hotmail.com>
2020-10-01 12:10:47 +0100

    added unit tests

--- includes/functions.php
+++ includes/functions.php
@@ -1,6 +1,24 @@
 <?php
 
-function display_card($player) {
+// array below refers to the input datatype and string refers to the output (return) datatype. With the malformed test we are testing the input datatype so all I have to do in the malformed test function in the tests/index file is pass the wrong datatype to $input
+
+function display_card(array $player): string  {
+    // This is for my failure test. Here I am saying that if any one of the keys(fields) in my database fails for some reason (like someone has changed the name) and the corresponding value cannot be output (in other words it is empty) then return an empty string (i.e. return nothing), otherwise just return the actual value.
+
+    if (
+        empty($player['club']) &&
+        empty($player['player_first_name']) &&
+        empty($player['player_last_name']) &&
+        empty($player['image']) &&
+        empty($player['position']) &&
+        empty($player['DOB']) &&
+        empty($player['height']) &&
+        empty($player['weight'])
+
+    ) {
+        return '';
+    }
+
     return '<div class="card ' . str_replace(' ', '_',$player['club']) . '">' .
             '<div class="card_top">' .
             '<h2 class="player_name">' . $player['player_first_name'] . ' ' . $player['player_last_name'] . '</h2>' .
@@ -16,10 +34,15 @@ function display_card($player) {
             '</div>';
 }
 
+// SUCCESS TEST
+
 // $input ['club' => dog, 'player_first_name' => cat, 'player_last_name' => bird, 'image' => deer, 'position' => rabbit, 'DOB' => turtle, 'height' => wolf, 'weight' => gorilla]
 
 // expected output '<div class="card_top"><h2 class="player_name">cat bird</h2><h3 class="club">dog </h3></div><img src="deer"><div class="stats"><p class="position">Position: rabbit </p><p class="DOB">DOB: turtle </p><p class="height">Height: wolf m </p><p class="weight">Weight: gorilla kg </p></div>'
 
+// From Terminal
+// What I gave it
 //'<div class="card_top"><h2 class="player_name">cat bird</h2><h3 class="club">dog </h3></div><img src="deer"><div class="stats"><p class="position">Position: rabbit </p><p class="DOB">DOB: turtle </p><p class="height">Height: wolf m </p><p class="weight">Weight: gorilla kg </p></div>'
-//
+
+// What it expected
 //+'<div class="card dog"><div class="card_top"><h2 class="player_name">cat bird</h2><h3 class="club">dog </h3></div><img src="deer"><div class="stats"><p class="position">Position: rabbit </p><p class="DOB">DOB: turtle </p><p class="height">Height: wolf m </p><p class="weight">Weight: gorilla kg </p></div></div>'
--- tests/index.php
+++ tests/index.php
@@ -5,9 +5,10 @@ require '../includes/functions.php';
 use PHPUnit\Framework\TestCase;
 
 class Index extends TestCase
-{
 
+{
     public function testSuccessDisplayCard()
+        // my input is an array and my expected output is a string
     {
         $input = ['club' => 'dog', 'player_first_name' => 'cat', 'player_last_name' => 'bird', 'image' => 'deer', 'position' => 'rabbit', 'DOB' => 'turtle', 'height' => 'wolf', 'weight' => 'gorilla'];
         $expectedOutput = '<div class="card dog"><div class="card_top"><h2 class="player_name">cat bird</h2><h3 class="club">dog </h3></div><img src="deer"><div class="stats"><p class="position">Position: rabbit </p><p class="DOB">DOB: turtle </p><p class="height">Height: wolf m </p><p class="weight">Weight: gorilla kg </p></div></div>';
@@ -16,25 +17,104 @@ class Index extends TestCase
 
         $this->assertEquals($expectedOutput, $result);
     }
-}
 
-//    public function testSuccessdoMaths2()
-//    {
-//        $input = 10;
-//        $expectedOutput = 4.0;
-//
-//        $result = doMaths($input);
-//
-//        $this->assertEquals($expectedOutput, $result);
-//    }
-//
-//    public function testMalformeddoMaths()
-//    {
-//        $input = [];
-//        $this->expectException(TypeError::class);
-//
-//        $result = doMaths($input);
-//
-//    }
-//
-//}
+    public function testFailureDisplayCard()
+
+    {
+        $input = ['klub' => 'dog', 'player_first_name' => 'cat', 'player_last_name' => 'bird', 'image' => 'deer', 'position' => 'rabbit', 'DOB' => 'turtle', 'height' => 'wolf', 'weight' => 'gorilla'];
+        $expectedOutput = '';
+
+        $result = display_card($input);
+
+        $this->assertEmpty($expectedOutput, $result);
+    }
+
+    public function testFailureDisplayCard2()
+
+    {
+        $input = ['club' => 'dog', 'playa_first_name' => 'cat', 'player_last_name' => 'bird', 'image' => 'deer', 'position' => 'rabbit', 'DOB' => 'turtle', 'height' => 'wolf', 'weight' => 'gorilla'];
+        $expectedOutput = '';
+
+        $result = display_card($input);
+
+        $this->assertEmpty($expectedOutput, $result);
+    }
+
+    public function testFailureDisplayCard3()
+
+    {
+        $input = ['club' => 'dog', 'player_first_name' => 'cat', 'playa_last_name' => 'bird', 'image' => 'deer', 'position' => 'rabbit', 'DOB' => 'turtle', 'height' => 'wolf', 'weight' => 'gorilla'];
+        $expectedOutput = '';
+
+        $result = display_card($input);
+
+        $this->assertEmpty($expectedOutput, $result);
+    }
+
+    public function testFailureDisplayCard4()
+
+    {
+        $input = ['club' => 'dog', 'player_first_name' => 'cat', 'player_last_name' => 'bird', 'img' => 'deer', 'position' => 'rabbit', 'DOB' => 'turtle', 'height' => 'wolf', 'weight' => 'gorilla'];
+        $expectedOutput = '';
+
+        $result = display_card($input);
+
+        $this->assertEmpty($expectedOutput, $result);
+    }
+
+    public function testFailureDisplayCard5()
+
+    {
+        $input = ['club' => 'dog', 'player_first_name' => 'cat', 'player_last_name' => 'bird', 'image' => 'deer', 'positon' => 'rabbit', 'DOB' => 'turtle', 'height' => 'wolf', 'weight' => 'gorilla'];
+        $expectedOutput = '';
+
+        $result = display_card($input);
+
+        $this->assertEmpty($expectedOutput, $result);
+    }
+
+    public function testFailureDisplayCard6()
+
+    {
+        $input = ['club' => 'dog', 'player_first_name' => 'cat', 'player_last_name' => 'bird', 'image' => 'deer', 'position' => 'rabbit', 'DAB' => 'turtle', 'height' => 'wolf', 'weight' => 'gorilla'];
+        $expectedOutput = '';
+
+        $result = display_card($input);
+
+        $this->assertEmpty($expectedOutput, $result);
+    }
+
+    public function testFailureDisplayCard7()
+
+    {
+        $input = ['club' => 'dog', 'player_first_name' => 'cat', 'player_last_name' => 'bird', 'image' => 'deer', 'position' => 'rabbit', 'DOB' => 'turtle', 'hight' => 'wolf', 'weight' => 'gorilla'];
+        $expectedOutput = '';
+
+        $result = display_card($input);
+
+        $this->assertEmpty($expectedOutput, $result);
+    }
+
+    public function testFailureDisplayCard8()
+
+    {
+        $input = ['club' => 'dog', 'player_first_name' => 'cat', 'player_last_name' => 'bird', 'image' => 'deer', 'position' => 'rabbit', 'DOB' => 'turtle', 'height' => 'wolf', 'weght' => 'gorilla'];
+        $expectedOutput = '';
+
+        $result = display_card($input);
+
+        $this->assertEmpty($expectedOutput, $result);
+    }
+
+   public function testMalformedDisplayCard()
+       // my datatype is a string but I have passed an integer
+   {
+       // For the malformed test we are essentially saying does the input data
+       $input = 10;
+       $this->expectException(TypeError::class);
+
+       $result = display_card($input);
+
+   }
+
+}
